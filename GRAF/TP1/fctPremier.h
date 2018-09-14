85dcb6dafc66d01469cc0d617272b4490622bc7f
bchanez <bastien.chanez@gmail.com>
2018-09-14 15:09:03 +0200

    file

---
+++ GRAF/TP1/fctPremier.h
@@ -0,0 +1,16 @@
+
+/***********************************************************************
+ * fichier : fctPremier.h
+ *
+ * auteur :  JM Nicod
+ * 
+ * resume :  Defini les fonctions externes
+ *
+ * date :    21/02/05
+ *
+ ***********************************************************************
+ */
+
+/* verification si un entier est premier ou non */
+extern int premier(int);
+
