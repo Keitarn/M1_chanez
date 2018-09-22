b756d2a3c26f34a371e46649deb8290b6b4cfcc9
keitarn <florian.joriot@hotmail.fr>
2018-09-22 14:35:00 +0200

    commit du changement de pointeur de pointeur de graph.c

--- GRAF/TP2/Graph.c
+++ GRAF/TP2/Graph.c
@@ -1,27 +1,52 @@
 #include <stdlib.h>
 #include "Graph.h"
 
-void createGraph(struct Graph **graph, int nbMaxNodes, bool isDirected) {
-    *graph = (struct Graph *) malloc(sizeof(struct Graph));
-    (*graph)->isDirected = isDirected;
-    (*graph)->nbMaxNodes = nbMaxNodes;
-    (*graph)->adjList = (struct Neighbour *) malloc((sizeof(struct Neighbour) * nbMaxNodes));
+void createGraph(struct Graph *graph, int nbMaxNodes, bool isDirected) {
+    graph = (struct Graph *) malloc(sizeof(struct Graph));
+    graph->isDirected = isDirected;
+    graph->nbMaxNodes = nbMaxNodes;
+    graph->adjList = (struct Neighbour *) malloc((sizeof(struct Neighbour) * nbMaxNodes));
 }
 
-void addNode(struct Graph **graph, int node) {
+void addNode(struct Graph *graph, int node) {
 
-    if (!(0 < node && node <= (*graph)->nbMaxNodes)) {
+    if (!(0 < node && node <= graph->nbMaxNodes)) {
         printf("ERROR\n");
         return;
     }
 
     struct Neighbour *neighbour = (struct Neighbour *) malloc(sizeof(struct Neighbour));
-    (*graph)->adjList[node - 1] = *neighbour;
+    graph->adjList[node - 1] = *neighbour;
 
 
 }
 
-void addEdge(struct Graph **graph, int from, char name, int to) {
+void addEdge(struct Graph *graph, int from, int weight, int to) {
+    if (from <= graph->nbMaxNodes | from > 0) {
+        if (to <= graph->nbMaxNodes | to > 0) {
+            if(weight > 0){
+                if ( graph->adjList[from-1] != NULL & (graph)->adjList[to-1] != NULL) {
+                    struct Neighbour *neighbour = (struct Neighbour *) malloc(sizeof(struct Neighbour));
+                    neighbour->neighbour = to;
+                    neighbour->weigh = weight;
+                    neighbour->nextNeighbour = (graph)->adjList[from - 1];
+                    neighbour->previousNeighbour = (graph)->adjList[from - 1].previousNeighbour;
+                    (graph)->adjList[from - 1].previousNeighbour = neighbour;
+                    (graph)->adjList[from - 1] = neighbour;
+                } else {
+                    fprintf(stderr, "ERROR : addNode() -> node , l'une des deux node n'a pas été ajouté");
+                }
+            } else {
+                fprintf(stderr, "ERROR : addNode() -> weight : %i, valeur du poids incorrect", weight);
+            }
+        } else {
+            fprintf(stderr, "ERROR : addNode() -> nodeTo : %i, valeur de la node incorrect", to);
+        }
+    } else {
+        fprintf(stderr, "ERROR : addNode() -> nodeFrom : %i, valeur de la node incorrect", from);
+    }
+
+
 }
 
 void removeNode(struct Graph **graph, int node) {
