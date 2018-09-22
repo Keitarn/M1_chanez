837302c091a7908bfd11b924fc767f499f12b43b
bchanez <bastien.chanez@gmail.com>
2018-09-22 17:20:32 +0200

    need fix addEdge

--- GRAF/TP2/Graph.c
+++ GRAF/TP2/Graph.c
@@ -22,32 +22,45 @@ void addNode(struct Graph **graph, int node) {
     neighbour->previousNeighbour = (struct Neighbour *) malloc(sizeof(struct Neighbour));
     neighbour->previousNeighbour = neighbour;
 
-   (*graph)->adjList[node - 1] = *neighbour;
+    (*graph)->adjList[node - 1] = *neighbour;
 }
 
 void addEdge(struct Graph **graph, int from, int weight, int to) {
     if (from > (*graph)->nbMaxNodes | from <= 0) {
-        fprintf(stderr, "ERROR : addNode() -> nodeFrom : %i, valeur de la node incorrect", from);
+        fprintf(stderr, "ERROR : addNode() -> nodeFrom : %i, valeur de la node incorrect\n", from);
     }
     if (to > (*graph)->nbMaxNodes | to <= 0) {
-        fprintf(stderr, "ERROR : addNode() -> nodeTo : %i, valeur de la node incorrect", to);
+        fprintf(stderr, "ERROR : addNode() -> nodeTo : %i, valeur de la node incorrect\n", to);
     }
     if (weight <= 0) {
-        fprintf(stderr, "ERROR : addNode() -> weight : %i, valeur du poids incorrect", weight);
+        fprintf(stderr, "ERROR : addNode() -> weight : %i, valeur du poids incorrect\n", weight);
     }
-    if (&(*graph)->adjList[from - 1] == NULL & &(*graph)->adjList[to - 1] == NULL) {
-        fprintf(stderr, "ERROR : addNode() -> node , l'une des deux node n'a pas été ajouté");
-    }
+//todo need fix
+//    if ((*graph)->adjList[from - 1] == NULL && (*graph)->adjList[to - 1] == NULL) {
+//        fprintf(stderr, "ERROR : addNode() -> node , l'une des deux node n'a pas été ajouté\n");
+//    }
 
-    struct Neighbour *neighbour = (struct Neighbour *) malloc(sizeof(struct Neighbour));
-    neighbour->neighbour = to;
-    neighbour->weigh = weight;
-   *neighbour->nextNeighbour = (*graph)->adjList[from - 1];
-    neighbour->previousNeighbour = (*graph)->adjList[from - 1].previousNeighbour;
-    (*graph)->adjList[from - 1].previousNeighbour = neighbour;
-    (*graph)->adjList[from - 1] = *neighbour;
+//todo there is a bug to fix
+    struct Neighbour *neighbourTo = (struct Neighbour *) malloc(sizeof(struct Neighbour));
+    neighbourTo->neighbour = to;
+    neighbourTo->weigh = weight;
+    neighbourTo->nextNeighbour = (struct Neighbour *) malloc(sizeof(struct Neighbour));
+    neighbourTo->nextNeighbour = &(*graph)->adjList[from - 1];
+    neighbourTo->previousNeighbour = (struct Neighbour *) malloc(sizeof(struct Neighbour));
+    neighbourTo->previousNeighbour = (*graph)->adjList[from - 1].previousNeighbour;
+
+    (*graph)->adjList[from - 1] = *neighbourTo;
 
 
+    struct Neighbour *neighbourFrom = (struct Neighbour *) malloc(sizeof(struct Neighbour));
+    neighbourFrom->neighbour = from;
+    neighbourFrom->weigh = weight;
+    neighbourFrom->nextNeighbour = (struct Neighbour *) malloc(sizeof(struct Neighbour));
+    neighbourFrom->nextNeighbour = &(*graph)->adjList[to - 1];
+    neighbourFrom->previousNeighbour = (struct Neighbour *) malloc(sizeof(struct Neighbour));
+    neighbourFrom->previousNeighbour = (*graph)->adjList[to - 1].previousNeighbour;
+
+    (*graph)->adjList[to - 1] = *neighbourFrom;
 }
 
 void removeNode(struct Graph **graph, int node) {
