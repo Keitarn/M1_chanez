2fe48395fb523c86288f37b17f7e7fef3fea6851
keitarn <florian.joriot@hotmail.fr>
2018-09-23 15:51:54 +0200

    Ajout test dans le main

--- GRAF/TP2/Graph.c
+++ GRAF/TP2/Graph.c
@@ -2,10 +2,10 @@
 #include "Graph.h"
 
 void createGraph(struct Graph *graph, int nbMaxNodes, bool isDirected) {
-   //* graph = (struct Graph *) malloc(sizeof(struct Graph));
+    //* graph = (struct Graph *) malloc(sizeof(struct Graph));
     graph->isDirected = isDirected;
     graph->nbMaxNodes = nbMaxNodes;
-    graph->adjList = (struct Neighbour **) malloc(graph->nbMaxNodes * sizeof(struct Neighbour));
+    graph->adjList = (struct Neighbour **) malloc(graph->nbMaxNodes * sizeof(struct Neighbour *));
 }
 
 void addNode(struct Graph *graph, int node) {
@@ -20,22 +20,22 @@ void addNode(struct Graph *graph, int node) {
     neighbour->nextNeighbour = neighbour;
     neighbour->previousNeighbour = neighbour;
     graph->adjList[node - 1] = neighbour;
+    printf("%i\n", graph->adjList[node-1]->weigh);
 }
 
 void addEdge(struct Graph *graph, int from, int weight, int to) {
     if (from > graph->nbMaxNodes | from <= 0) {
-        fprintf(stderr, "ERROR : addNode() -> nodeFrom : %i, valeur de la node incorrect\n", from);
+        fprintf(stderr, "ERROR : addEdge() -> nodeFrom : %i, valeur de la node incorrect\n", from);
         return;
     }
     if (to > graph->nbMaxNodes | to <= 0) {
-        fprintf(stderr, "ERROR : addNode() -> nodeTo : %i, valeur de la node incorrect\n", to);
+        fprintf(stderr, "ERROR : addEdge() -> nodeTo : %i, valeur de la node incorrect\n", to);
         return;
     }
     if (weight <= 0) {
-        fprintf(stderr, "ERROR : addNode() -> weight : %i, valeur du poids incorrect\n", weight);
+        fprintf(stderr, "ERROR : addEdge() -> weight : %i, valeur du poids incorrect\n", weight);
         return;
     }
-    printf("le nombre max de node est : %i\n",graph->nbMaxNodes);
 
     if ((graph->adjList[from - 1]) != NULL) {
         if ((graph->adjList[to - 1]) != NULL) {
@@ -44,14 +44,15 @@ void addEdge(struct Graph *graph, int from, int weight, int to) {
             neighbour->weigh = weight;
             neighbour->nextNeighbour = graph->adjList[from - 1];
             neighbour->previousNeighbour = graph->adjList[from - 1]->previousNeighbour;
+            graph->adjList[from - 1]->previousNeighbour->nextNeighbour = neighbour;
             graph->adjList[from - 1]->previousNeighbour = neighbour;
             graph->adjList[from - 1] = neighbour;
         } else {
-            fprintf(stderr, "ERROR : addNode() -> node , l'une des deux node n'a pas été ajouté\n");
+            fprintf(stderr, "ERROR : addEdge() -> node , la nodeFrom n'a pas été ajouté\n");
             return;
         }
     } else {
-        fprintf(stderr, "ERROR : addNode() -> node , l'une des deux node n'a pas été ajouté\n");
+        fprintf(stderr, "ERROR : addEdge() -> node , la nodeTo n'a pas été ajouté\n");
         return;
     }
 
