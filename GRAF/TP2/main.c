720ccbc500fe18f5dae312c649619f07c2318790
keitarn <florian.joriot@hotmail.fr>
2018-09-23 15:21:17 +0200

    Ajout test dans le main

--- GRAF/TP2/main.c
+++ GRAF/TP2/main.c
@@ -9,10 +9,11 @@ int main() {
     addNode(&graph, 0); // error
     addNode(&graph, 1);
     addNode(&graph, 2);
-    addNode(&graph, 3);
     addNode(&graph, 4);
     addNode(&graph, 5); // error
     addEdge(&graph,1,40,2);
+    addEdge(&graph,1,40,3);// error
+    addEdge(&graph,3,40,4);// error
     addEdge(&graph,0,40,2); // error
     return 0;
 }
