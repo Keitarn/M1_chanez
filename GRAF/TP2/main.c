7fb76db9127881b6103f1d29ed55ea8448c85fd4
bchanez <bastien.chanez@gmail.com>
2018-10-01 19:59:52 +0200

    changement main

--- GRAF/TP2/main.c
+++ GRAF/TP2/main.c
@@ -10,7 +10,7 @@ int main() {
 //    createGraph(&graph, -1, true); // error
 //    createGraph(&graph, 0, true); // error
     createGraph(&graph, 8, true);
-//    createGraph(&graph, 4, true);
+    createGraph(&graph, 4, true);
 
     addNode(graph, 1);
     addNode(graph, 2);
@@ -19,14 +19,13 @@ int main() {
 //    addNode(graph1, 1); // error
 //    addNode(graph, 0); // error
 //    addNode(graph, 5); // error
-//    addNode(graph, 3); // error
 
     addEdge(graph, 1, 11, 1);
     addEdge(graph, 1, 12, 2);
     addEdge(graph, 2, 12, 1);
-    addEdge(graph, 1, 13, 4); //coredump
+    addEdge(graph, 1, 13, 4);
 
-    addEdge(graph1, 1, 13, 4);
+    addEdge(graph1, 1, 13, 4); // error
     addEdge(graph, 0, 14, 5); // error
     addEdge(graph, 1, -1, 4); // error
     addEdge(graph, 1, 40, 3); // error
@@ -48,16 +47,16 @@ int main() {
 //    removeNode(graph, 0); // error
 //    removeNode(graph, 5); // error
 
-    addEdge(graph1, 1, 13, 4); // error
+//    addEdge(graph1, 1, 13, 4); // error
 //
-//    viewGraph(graph);
+    viewGraph(graph);
 //
-    quit(graph1); // error
+//    quit(graph1); // error
 
-//    saveGraph(graph, "graph.txt");
+    saveGraph(graph, "graph.txt");
 //    saveGraph(graph1, "graph.txt"); // error
 //
-//    loadGraph(graph1, "graph.txt");
+//    loadGraph(graph1, "graph.txt"); // error
 //    loadGraph(graph1, "graph1.txt"); // error
 
 //    viewGraph(graph1);
