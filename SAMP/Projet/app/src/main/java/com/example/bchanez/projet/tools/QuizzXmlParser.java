48fbe2ea0d03730fa698beecaa7f49d85abdd51e
bchanez <bastien.chanez@gmail.com>
2018-11-03 17:08:03 +0100

    samp project end parser xml

--- SAMP/Projet/app/src/main/java/com/example/bchanez/projet/tools/QuizzXmlParser.java
+++ SAMP/Projet/app/src/main/java/com/example/bchanez/projet/tools/QuizzXmlParser.java
@@ -1,18 +1,22 @@
 package com.example.bchanez.projet.tools;
 
 import android.content.Context;
-import android.util.Log;
 import android.util.Xml;
 
 import com.example.bchanez.projet.bdd.QuestionManager;
 import com.example.bchanez.projet.bdd.QuizzManager;
 import com.example.bchanez.projet.bdd.ReponseManager;
+import com.example.bchanez.projet.model.Question;
+import com.example.bchanez.projet.model.Quizz;
+import com.example.bchanez.projet.model.Reponse;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class QuizzXmlParser {
     private static final String ns = null;
@@ -52,8 +56,6 @@ public class QuizzXmlParser {
     private void readQuizzs(XmlPullParser parser) throws XmlPullParserException, IOException {
         parser.require(XmlPullParser.START_TAG, ns, "Quizzs");
 
-//        Log.d("log-parser", parser.getName());
-
         while (!(parser.next() == XmlPullParser.END_TAG && parser.getName().equals("Quizzs"))) {
             if (parser.getEventType() != XmlPullParser.START_TAG) {
                 continue;
@@ -75,7 +77,10 @@ public class QuizzXmlParser {
     private void readQuizz(XmlPullParser parser) throws XmlPullParserException, IOException {
         parser.require(XmlPullParser.START_TAG, ns, "Quizz");
 
-//        Log.d("log-parser", parser.getName() + " " + parser.getAttributeValue(ns, "type"));
+        Quizz quizz = new Quizz(-1, parser.getAttributeValue(ns, "type"));
+        quizzManager.open();
+        quizz.setId((int) quizzManager.addQuizz(quizz));
+        quizzManager.close();
 
         while (!(parser.next() == XmlPullParser.END_TAG && parser.getName().equals("Quizz"))) {
             if (parser.getEventType() != XmlPullParser.START_TAG) {
@@ -84,7 +89,7 @@ public class QuizzXmlParser {
 
             switch (parser.getName()) {
                 case "Question":
-                    readQuestion(parser);
+                    readQuestion(parser, quizz);
                     break;
                 default:
                     skip(parser);
@@ -95,10 +100,16 @@ public class QuizzXmlParser {
         parser.require(XmlPullParser.END_TAG, ns, "Quizz");
     }
 
-    private void readQuestion(XmlPullParser parser) throws XmlPullParserException, IOException {
+    private void readQuestion(XmlPullParser parser, Quizz quizz) throws XmlPullParserException, IOException {
         parser.require(XmlPullParser.START_TAG, ns, "Question");
 
-//        Log.d("log-parser", parser.getName() + " " + readText(parser));
+        Question question = new Question(-1, readText(parser), quizz);
+        questionManager.open();
+        question.setId((int) questionManager.addQuestion(question));
+        questionManager.close();
+
+        List reponses = new ArrayList();
+        int valeur = -1;
 
         while (!(parser.next() == XmlPullParser.END_TAG && parser.getName().equals("Question"))) {
             if (parser.getEventType() != XmlPullParser.START_TAG) {
@@ -107,10 +118,10 @@ public class QuizzXmlParser {
 
             switch (parser.getName()) {
                 case "Propositions":
-                    readPropositions(parser);
+                    reponses = readPropositions(parser);
                     break;
                 case "Reponse":
-                    readReponse(parser);
+                    valeur = readReponse(parser);
                     break;
                 default:
                     skip(parser);
@@ -119,13 +130,22 @@ public class QuizzXmlParser {
         }
 
         parser.require(XmlPullParser.END_TAG, ns, "Question");
+
+        ((Reponse) reponses.get(valeur - 1)).setVrai(true);
+
+        for (Object reponse : reponses) {
+            ((Reponse) reponse).setQuestion(question);
+            reponseManager.open();
+            reponseManager.addReponse((Reponse) reponse);
+            reponseManager.close();
+        }
     }
 
 
-    private void readPropositions(XmlPullParser parser) throws XmlPullParserException, IOException {
+    private List readPropositions(XmlPullParser parser) throws XmlPullParserException, IOException {
         parser.require(XmlPullParser.START_TAG, ns, "Propositions");
 
-        Log.d("log-parser", parser.getName());
+        List reponses = new ArrayList();
 
         while (!(parser.next() == XmlPullParser.END_TAG && parser.getName().equals("Propositions"))) {
             if (parser.getEventType() != XmlPullParser.START_TAG) {
@@ -134,7 +154,7 @@ public class QuizzXmlParser {
 
             switch (parser.getName()) {
                 case "Proposition":
-                    readProposition(parser);
+                    reponses.add(readProposition(parser));
                     break;
                 default:
                     skip(parser);
@@ -143,20 +163,25 @@ public class QuizzXmlParser {
         }
 
         parser.require(XmlPullParser.END_TAG, ns, "Propositions");
+
+        return reponses;
     }
 
-    private void readProposition(XmlPullParser parser) throws XmlPullParserException, IOException {
+    private Reponse readProposition(XmlPullParser parser) throws XmlPullParserException, IOException {
         parser.require(XmlPullParser.START_TAG, ns, "Proposition");
-        Log.d("log-parser", parser.getName() + " " + readText(parser));
+        Reponse reponse = new Reponse(-1, readText(parser), false, null);
         parser.nextTag();
         parser.require(XmlPullParser.END_TAG, ns, "Proposition");
+        return reponse;
     }
 
-    private void readReponse(XmlPullParser parser) throws XmlPullParserException, IOException {
+    private int readReponse(XmlPullParser parser) throws XmlPullParserException, IOException {
         parser.require(XmlPullParser.START_TAG, ns, "Reponse");
-        Log.d("log-parser", parser.getAttributeValue(ns, "valeur"));
+        int valeur = Integer.parseInt(parser.getAttributeValue(ns, "valeur"));
         parser.nextTag();
         parser.require(XmlPullParser.END_TAG, ns, "Reponse");
+
+        return valeur;
     }
 
     private String readText(XmlPullParser parser) throws IOException, XmlPullParserException {
